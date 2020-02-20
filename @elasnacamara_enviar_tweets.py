9149dc578a1e13625b544991e00bc3d1dde533d0
Renato Cron <renato.cron@gmail.com>
2020-02-20 17:19:19 -0300

    Adicionando @elasnacamara_enviar_tweets.py (enviar os tweets), requirements.txt (deps), .env.sample exemplo do .env

---
+++ @elasnacamara_enviar_tweets.py
@@ -0,0 +1,63 @@
+import tweepy, time, sys
+import hashlib  # hash do tweet
+import json     # escrever JSON
+import os.path  # paths do sistema
+import os       # ler variaveis de ambiente
+import time     # sleep
+from dotenv import load_dotenv # ler variaveis de ambiente do arquivo .env
+
+load_dotenv()
+
+if not os.path.exists('dados/.dummy'):
+    print("Faltando pasta ./dados/ (arquivo .dummy nao existe)")
+    raise
+
+if not os.path.exists('dados/tweets.json'):
+    print("Faltando arquivo ./dados/tweets.json [execute primeiro o script @elasnacamara.py")
+    raise
+
+if not os.getenv("CONSUMER_KEY"):
+    print("Faltando configurar ENV CONSUMER_KEY")
+    raise
+if not os.getenv("CONSUMER_SECRET"):
+    print("Faltando configurar ENV CONSUMER_SECRET")
+    raise
+if not os.getenv("ACCESS_KEY"):
+    print("Faltando configurar ENV ACCESS_KEY")
+    raise
+if not os.getenv("ACCESS_SECRET"):
+    print("Faltando configurar ENV ACCESS_SECRET")
+    raise
+
+
+dirName = 'dados/tweets-enviados/'
+if not os.path.exists(dirName):
+    os.mkdir(dirName)
+    print("Diretorio " , dirName ,  " criado")
+
+with open('dados/tweets.json') as f:
+  tweets = json.load(f)
+
+auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET"))
+auth.set_access_token(os.getenv("ACCESS_KEY"), os.getenv("ACCESS_SECRET"))
+api = tweepy.API(auth)
+
+for tweet in tweets:
+    text = tweet['tweet']
+    result = hashlib.md5(text.encode())
+    fileName = f'{dirName}/{result.hexdigest()}'
+
+    if os.path.exists( fileName ):
+        print ("tweet '", text, "' ja foi tweetado")
+        continue
+
+    print ("tweetando '", text, "'...")
+    response = api.update_status(text)
+    with open(fileName, 'w') as outfile:
+        json.dump(response._json, outfile)
+
+    if bool(os.getenv("RANDOM_SLEEP_BETWEEN_TWEETS")):
+        time.sleep(300) # espera 5 minutos
+
+
+print ("Todos tweets enviados com sucesso!")
--- README.md
+++ README.md
@@ -9,3 +9,26 @@ Os termos para a busca na ementa e a URL com data de início e fim da busca pode
 
 Esse projeto foi elaborado como trabalho de conclusão de curso do MOOC "Introdução à programação: Python para jornalistas", oferecido pelo Centro Knight com o apoio do Google News Lab.
 
+# instalando dependencias
+
+> Primeiro verificar se o pip para o python3 está instalado (sudo apt install python3-pip em debian-like)
+
+    $ python3 -m pip --version
+
+Deve exibir: `pip 9.0.1 from /usr/lib/python3/dist-packages (python 3.6)` ou semelhante
+
+    $ python3 -m pip install -r requirements.txt
+
+# executando
+
+Para baixar as proposições, basta executar o arquivo `@elasnacamara.py`, o resultado dos tweets será salvo em ./dados/tweets.json
+
+Caso esteja tudo OK com o conteudo, você pode deve executar o script `@elasnacamara_enviar_tweets.py` para fazer o envio.
+
+Para executar `@elasnacamara_enviar_tweets.py`, será necessário configurar o arquivo .env com a chave do twitter:
+
+    $ cp .env.sample .env
+
+Edite o conteudo do .env com a chave, e então poderá ser possível executar o envio.
+
+A cada envio de tweet, um arquivo cujo nome é o hash do tweet, ficará salvo em ./dados/tweets-enviados/$HASH, dentro dentro deste arquivo, tem informações sobre o tweet.
