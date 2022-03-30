71f32d41331355078bad0ffe73565d4ee07cfe9f
Renato Cron <renato.cron@gmail.com>
2022-03-30 15:31:17 -0300

    Add tests for None

--- normalize_tweets_test.py
+++ normalize_tweets_test.py
@@ -2,7 +2,7 @@ import unittest
 import normalize_tweets as n
 
 class TestSum(unittest.TestCase):
-    def test_text(self):
+    def test_names(self):
         self.assertEqual(n.norm(' tem trim '), 'tem trim')
 
         self.assertEqual(n.norm(
@@ -20,6 +20,25 @@ class TestSum(unittest.TestCase):
         self.assertEqual(n.norm(
             'CÂMARA: PL 2753/2020 outros campo grandes demais, de autoria de qualquer texto aqui tambem vai ser cortado caso nao tenha como ir até oSofreAlteracoes sofreu alterações em sua tramitação. Tramitação: Devolução ao Relator. Situação: Aguardando Parecer. http://www.camara.gov.br/proposicoesWeb/prop_mostrarintegra?codteor=1895093 '),
             'CÂMARA: PL 2753/2020 outros campo grandes demais, Tramitação: Devolução ao Relator. Situação: Aguardando Parecer. http://www.camara.gov.br/proposicoesWeb/prop_mostrarintegra?codteor=1895093')
+
+    def test_nones(self):
+
+        self.assertEqual(n.norm(
+            'SENADO: PL 01360/2021, de autoria de None, fala sobre violência doméstica e sofreu alterações em sua tramitação. Tramitação: None. Situação: None. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187'),
+            'SENADO: PL 01360/2021 fala sobre violência doméstica e sofreu alterações em sua tramitação. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187')
+
+        self.assertEqual(n.norm(
+            'SENADO: PL 01360/2021, de autoria de Foo, fala sobre violência doméstica e sofreu alterações em sua tramitação. Tramitação: None. Situação: None. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187'),
+            'SENADO: PL 01360/2021, de autoria de Foo, fala sobre violência doméstica e sofreu alterações em sua tramitação. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187')
+
+        self.assertEqual(n.norm(
+            'SENADO: PL 01360/2021, de autoria de Foo, fala sobre violência doméstica e sofreu alterações em sua tramitação. Tramitação: Em Progress. Situação: None. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187'),
+            'SENADO: PL 01360/2021, de autoria de Foo, fala sobre violência doméstica e sofreu alterações em sua tramitação. Tramitação: Em Progress. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187')
+
+        self.assertEqual(n.norm(
+            'SENADO: PL 01360/2021, de autoria de Foo, fala sobre violência doméstica e sofreu alterações em sua tramitação. Tramitação: Em Progress. Situação: Xpto. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187'),
+            'SENADO: PL 01360/2021, de autoria de Foo, fala sobre violência doméstica e sofreu alterações em sua tramitação. Tramitação: Em Progress. Situação: Xpto. http://legis.senado.leg.br/sdleg-getter/documento?dm=8993187')
+
 
 if __name__ == '__main__':
     unittest.main()
