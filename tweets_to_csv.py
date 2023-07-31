5ecf4e961bb27c98ef5c2dc4b2b1d25d8bbb33e6
lucasansei <lucas.ansei@eokoe.com>
2023-07-31 09:26:36 -0300

    Omitindo coluna de hash

--- tweets_to_csv.py
+++ tweets_to_csv.py
@@ -13,6 +13,9 @@ client = gspread.authorize(creds)
 sheet = client.open('Teste').sheet1
 
 try:
+    # Read the existing CSV file into a DataFrame
+    existing_df = pd.read_csv('output.csv')
+    
     # Read the JSON file
     with open('dados/tweets.json', 'r') as f:
         data = json.load(f)
@@ -23,15 +26,25 @@ try:
     # Convert the list of dictionaries to a DataFrame
     df = pd.DataFrame(tweets)
     
-    # Convert the DataFrame to CSV data
-    csv_data = df.to_csv('output.csv', index=False)
+    # Remove rows from df that have a hash that exists in existing_df
+    df = df[~df['hash'].isin(existing_df['hash'])]
+    
+    # Convert the DataFrame to CSV data and append it to the existing CSV file
+    with open('output.csv', 'a') as f:
+        df.to_csv(f, header=f.tell()==0, index=False)
+    
+    # Read the CSV file
+    with open('output.csv', 'r') as f:
+        csv_data = f.read()
     
     # Use the csv module to split the lines into cells
     csv_reader = csv.reader(csv_data.splitlines())
     csv_cells = list(csv_reader)
     
+    # Remove the hash column
+    csv_cells = [row[:-1] for row in csv_cells]
+    
     # Clear all existing data in the sheet and upload new data
-    sheet.clear()
     sheet.append_rows(csv_cells)
 except ValueError as e:
     print(f"Error while reading JSON: {e}")
