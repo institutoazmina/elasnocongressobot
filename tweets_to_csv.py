eb257d4b208d92c90f9159062d3f5fd770587b85
lucasansei <lucas.ansei@eokoe.com>
2023-07-31 12:26:43 -0300

    Adicionando envs para GSheets

--- tweets_to_csv.py
+++ tweets_to_csv.py
@@ -2,15 +2,27 @@ import pandas as pd
 import gspread
 import json
 import csv
+from dotenv import load_dotenv # ler variaveis de ambiente do arquivo .env
 from oauth2client.service_account import ServiceAccountCredentials
+load_dotenv()
+
+
+if not os.getenv("GOOGLE_JSON_KEY"):
+    print("Faltando configurar ENV CONSUMER_KEY")
+    raise
+if not os.getenv("SPREADSHEET_NAME"):
+    print("Faltando configurar ENV CONSUMER_SECRET")
+    raise
+
 
 # Use the JSON key file you downloaded to authenticate and create an API client
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('dados/appcivico-teste-6a9ded47ac8d.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.getenv("GOOGLE_JSON_KEY"), scope)
 client = gspread.authorize(creds)
 
 # Open the Google Spreadsheet (replace 'My test sheet' with your sheet's name)
-sheet = client.open('Teste').sheet1
+## TODO change sheet matching the year.
+sheet = client.open(os.getenv("SPREADSHEET_NAME")).sheet1
 
 try:
     # Try to read the existing CSV file into a DataFrame
@@ -56,8 +68,17 @@ try:
         # Remove the hash column
         csv_cells = [row[:-1] for row in csv_cells]
         
-        # Clear all existing data in the sheet and upload new data
-        sheet.append_rows(csv_cells)
+        # Fetch the existing data from the sheet
+        existing_data = sheet.get_all_values()
+        
+        # Prepend the new rows to the existing data
+        updated_data = csv_cells + existing_data[1:]  # Exclude the old header
+        
+        # Clear the sheet
+        sheet.clear()
+        
+        # Append the updated data back to the sheet
+        sheet.append_rows(updated_data)
 except ValueError as e:
     print(f"Error while reading JSON: {e}")
 except Exception as e:
