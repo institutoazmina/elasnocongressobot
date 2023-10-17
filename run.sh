7a8292e238ae88bd3c56988c44273bbf8bf1df3e
lucasansei <lucas.ansei@eokoe.com>
2023-10-17 16:30:44 -0300

    Fix: typo

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 date >> logs/docker.elasnocongresso.log
 cd src/
-python3 -m scrapy crawl camara 1>> logs/crawl_camara.log 2>> logs/crawl_camara_.err.log;
-python3 -m scrapy crawl senado 1>> logs/crawl_senado.log 2>> logs/crawl_senado_.err.log;
+python3 -m scrapy crawl camara 1>> ../logs/crawl_camara.log 2>> ../logs/crawl_camara.err.log;
+python3 -m scrapy crawl senado 1>> ../logs/crawl_senado.log 2>> ../logs/crawl_senado.err.log;
 python sync_spreadsheet.py
 
