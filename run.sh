1e7d82d4440d7e4fcb2df249d4a500e7c4e23377
belisards <adrianobf@gmail.com>
2025-01-15 17:47:50 -0300

    merge scripts for both models

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ date >> logs/docker.elasnocongresso.log
 cd src/
 python3 -m scrapy crawl camara 1>> ../logs/crawl_camara.log 2>> ../logs/crawl_camara.err.log;
 python3 -m scrapy crawl senado 1>> ../logs/crawl_senado.log 2>> ../logs/crawl_senado.err.log;
-python predict_tema.py
+python run_models.py
 python sync_spreadsheets.py
 
